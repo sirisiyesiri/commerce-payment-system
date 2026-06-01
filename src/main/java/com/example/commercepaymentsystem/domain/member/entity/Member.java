a504568a580dc3bce497e6e66f23d4aa58f108df
ChoiT815 <choi081517@gmail.com>
2026-06-01 12:32:02 +0900

    feat: Member 엔티티 추가
    - Member 엔티티 생성
    - id, email,
      password, name,
      phone, role 필드
    - MemberRole enum
      추가

--- src/main/java/com/example/commercepaymentsystem/domain/member/entity/Member.java
+++ src/main/java/com/example/commercepaymentsystem/domain/member/entity/Member.java
@@ -1,4 +1,44 @@
 package com.example.commercepaymentsystem.domain.member.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberRole role;
+
+    @Builder
+    public Member(String email, String password, String name, String phone, MemberRole role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.role = role;
+    }
+
 }

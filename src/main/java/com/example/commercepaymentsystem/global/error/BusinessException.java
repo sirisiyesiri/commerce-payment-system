7e77cc99b89d0c1e9a12f3e61771d8588b856cdf
ChoiT815 <choi081517@gmail.com>
2026-06-01 19:33:44 +0900

    refactor : BusinessException 수정

--- src/main/java/com/example/commercepaymentsystem/global/error/BusinessException.java
+++ src/main/java/com/example/commercepaymentsystem/global/error/BusinessException.java
@@ -5,12 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class BusinessException extends RuntimeException {
-    private final HttpStatus status;
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
-        this.status = errorCode.getStatus();
         this.errorCode = errorCode;
     }
 }

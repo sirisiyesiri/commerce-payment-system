ade575341972e8f7dee198b018dd0e1591df6216
ChoiT815 <choi081517@gmail.com>
2026-06-01 12:52:09 +0900

    feat: 공통 응답/예외 처리 추가
    - ApiResponse 추가
    - CustomException 추가
    - GlobalExceptionHandler
      추가

--- src/main/java/com/example/commercepaymentsystem/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/commercepaymentsystem/domain/member/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.example.commercepaymentsystem.domain.member.repository;
 
-public interface MemberRepository {
+import com.example.commercepaymentsystem.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/example/commercepaymentsystem/global/response/ApiResponse.java
+++ src/main/java/com/example/commercepaymentsystem/global/response/ApiResponse.java
@@ -1,4 +1,29 @@
 package com.example.commercepaymentsystem.global.response;
 
-public class ApiResponse {
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+
+    private final int status;
+    private final String message;
+    private final T data;
+
+    private ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T>ApiResponse<T> of(int status, String message, T data) {
+        return new ApiResponse<T>(status, message, data);
+    }
+
+    public static <T>ApiResponse<T> ok(String message, T data) {
+        return new ApiResponse<T>(200, message, data);
+    }
+
+    public static <T> ApiResponse<T> created(String message, T data) {
+        return new ApiResponse<>(201, message, data);
+    }
 }

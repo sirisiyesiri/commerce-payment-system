472474c30145abd61df06144820eab5e5101db60
ChoiT815 <choi081517@gmail.com>
2026-06-01 13:29:48 +0900

    feat: SecurityConfig 추가
    - CSRF 비활성화
    - Stateless 세션 설정
    - 인증 경로 설정
    - JwtAuthFilter 등록

--- src/main/java/com/example/commercepaymentsystem/global/config/SecurityConfig.java
+++ src/main/java/com/example/commercepaymentsystem/global/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package com.example.commercepaymentsystem.global.config;
 
+import com.example.commercepaymentsystem.global.fillter.JwtAuthFilter;
+import com.example.commercepaymentsystem.global.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }

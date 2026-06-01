bd4b236cd80c4a327d2cc32ca2aab510b80eb032
ChoiT815 <choi081517@gmail.com>
2026-06-01 19:06:37 +0900

    feat: JwtAuthFilter 예외 처리 추가
    - 유효하지 않은 토큰
      401 응답 처리
    - role 필드 제거

--- src/main/java/com/example/commercepaymentsystem/domain/member/controller/UserController.java
+++ src/main/java/com/example/commercepaymentsystem/domain/member/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor

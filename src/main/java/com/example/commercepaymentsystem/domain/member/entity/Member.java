25692658d8cf218265767e3eed661704789627aa
ChoiT815 <choi081517@gmail.com>
2026-06-01 14:58:40 +0900

    feat: Member 엔티티 수정
    - 테이블명 users로 변경
    - createdAt, modifiedAt 추가
    - 컬럼 길이 제한 추가
    - EnableJpaAuditing 추가

--- src/main/java/com/example/commercepaymentsystem/domain/member/entity/Member.java
+++ src/main/java/com/example/commercepaymentsystem/domain/member/entity/Member.java
@@ -1,36 +1,47 @@
 package com.example.commercepaymentsystem.domain.member.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "members")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 30)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false,  length = 30)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false,   length = 50)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
     private String phone;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private MemberRole role;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 
     @Builder
     public Member(String email, String password, String name, String phone, MemberRole role) {
